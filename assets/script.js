6fe89ee9eb87b24ceb3adc32d5db4ec659fda3de
Ibrahim-Smadi <96455066+Ibrahim-Smadi@users.noreply.github.com>
2022-01-26 18:42:36 -0500

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -7,6 +7,7 @@ let yourName = "Ibrahim Smadi" // HINT: Replace this with your own name!
 let gb = 0 // Gingerbread
 let cc = 0 // Chocolate Chip
 let sugar = 0 // Sugar Sprinkle
+let total = 0
 
 // selecting the element with an id of credit
 const credit = document.querySelector('#credit')
@@ -17,39 +18,53 @@ const ccPlusBtn = document.querySelector('#add-cc')
 const ccMinusBtn = document.querySelector('#minus-cc')
 const sugarPlusBtn = document.querySelector('#add-sugar')
 const sugarMinusBtn = document.querySelector('#minus-sugar')
+const qtyGb  = document.querySelector('#qty-gb')
+const qtyCc  = document.querySelector('#qty-cc')
+const qtySugar  = document.querySelector('#qty-sugar')
+const qtyTotal = document.querySelector('#qty-total')
 
 // Code to update name display
 credit.textContent = `Created By ${yourName}`
 
 // Event listener for clicks on the "+" button for Gingerbread cookies
 gbPlusBtn.addEventListener('click', function() {
-    g++;
-
-    
-// HINT: You can delete this console.log after you no longer need it!
-    console.log('Gingerbread + button was clicked!', gb++)
-
-// TODO: Write the code to be run when the "+" button for "Gingerbread" is clicked
+    console.log('Gingerbread + button was clicked!')
+    gb = gb + 1
+    qtyGb.textContent = ` ${gb}`
+    qtyTotal.textContent = gb + cc + sugar
 })
 gbMinusBtn.addEventListener('click', function() {
     console.log('Gingerbread - button was clicked!')
-    gb--
+    gb = gb - 1 
+    qtyGb.textContent = ` ${gb}`
+    qtyTotal.textContent = gb + cc + sugar
+
 })
 ccPlusBtn.addEventListener('click', function() {
+    cc = cc + 1
+    qtyCc.textContent = ` ${cc}`
+    qtyTotal.textContent = gb + cc + sugar
     console.log('Chocolate Chip + button was clicked!')
 
 })
 ccMinusBtn.addEventListener('click', function() {
+    cc = cc - 1
+    qtyCc.textContent = ` ${cc}`
+    qtyTotal.textContent = gb + cc + sugar
     console.log('Chocolate Chip - button was clicked!')
 
 })
 sugarPlusBtn.addEventListener('click', function() {
+    sugar = sugar + 1
+    qtySugar.textContent = `${sugar}`
+    qtyTotal.textContent = gb + cc + sugar
     console.log('Sugar + button was clicked!')
 
 })
 sugarMinusBtn.addEventListener('click', function() {
+    sugar = sugar - 1
+    qtySugar.textContent = `${sugar}`
+    qtyTotal.textContent = gb + cc + sugar
     console.log('Sugar - button was clicked!')
 
 })
-
-// TODO: Hook up event listeners for the rest of the buttons
